38a8aeb5064efd649e2cd787161a6745544ff1a1
Hiroya-W <hyuyukun@gmail.com>
2022-02-20 15:44:40 +0900

    :sprakles: Define ALL_PREFIXES

--- myxremap/new_keys.py
+++ myxremap/new_keys.py
@@ -1,9 +1,23 @@
-from enum import IntFlag, auto
+from enum import Flag, auto
+import itertools
+from typing import List
 
 
-class Modifier(IntFlag):
+class Modifier(Flag):
     NONE = 0
     CTRL = auto()
     ALT = auto()
     SHIFT = auto()
     SUPER = auto()
+
+
+_MODIFIER_LIST = list(Modifier)[1:]
+
+ALL_PREFIXES: List[Modifier] = [Modifier.NONE]
+
+for n in range(1, len(_MODIFIER_LIST) + 1):
+    for combination in itertools.combinations(_MODIFIER_LIST, n):
+        prefix = Modifier.NONE
+        for mod in combination:
+            prefix |= mod
+        ALL_PREFIXES.append(prefix)
