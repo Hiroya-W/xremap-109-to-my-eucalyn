7779d7c8b9310ff5e3dd4f06105c47f303fd0fab
Hiroya-W <hyuyukun@gmail.com>
2022-02-20 13:12:40 +0900

    :recycle: Add Key class

--- myxremap/new_types.py
+++ myxremap/new_types.py
@@ -1,11 +1,28 @@
-from typing import Dict, NamedTuple, NewType
+from typing import Dict, NewType
 
 from myxremap.new_keys import Modifier
 
 
-class Key(NamedTuple):
-    key: str
-    modifiers: Modifier = Modifier.NONE
+class Key:
+    def __init__(self, key: str, modifiers: Modifier = Modifier.NONE):
+        self.key = key
+        self.modifiers = modifiers
+
+    def __str__(self) -> str:
+        if self.modifiers == Modifier.NONE:
+            return self.key
+
+        prefixes = []
+        if self.modifiers == Modifier.CTRL:
+            prefixes.append("CTRL")
+        if self.modifiers == Modifier.ALT:
+            prefixes.append("ALT")
+        if self.modifiers == Modifier.SHIFT:
+            prefixes.append("SHIFT")
+        if self.modifiers == Modifier.SUPER:
+            prefixes.append("SUPER")
+
+        return "-".join(prefixes) + "-" + self.key
 
 
 KeyMap = NewType("KeyMap", Dict[Key, Dict[Modifier, Key]])
