70418e6eb279ad3d1db7ea4a30dbada1a8fb6bf9
Hiroya-W <hyuyukun@gmail.com>
2022-02-20 16:23:58 +0900

    :sparkles: Implemented InvalidKeySwapArgumentException

---
+++ myxremap/exceptions.py
@@ -0,0 +1,18 @@
+from myxremap.new_types import Key
+
+
+class XremapException(Exception):
+    def __init__(self, msg: str = "") -> None:
+        self.msg = msg
+
+
+class InvalidKeySwapArgumentException(XremapException):
+    def __init__(self, key: Key, msg: str = "") -> None:
+        super().__init__(msg)
+        self.key = key
+
+    def __str__(self) -> str:
+        exception_msg = "InvalidKeySwapArgumentException: \n"
+        exception_msg += "  " + self.msg + "\n"
+        exception_msg += "  " + str(self.key) + "\n"
+        return exception_msg
