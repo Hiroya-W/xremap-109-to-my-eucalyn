16b95eee04606757db66b1cbd29eb44006c3e3f1
Hiroya-W <hyuyukun@gmail.com>
2022-02-20 17:11:58 +0900

    :wastebasket: Remove old scripts

--- myxremap/converter.py
+++
@@ -1,8 +0,0 @@
-from myxremap.types import Key
-
-
-def key2str(key: Key) -> str:
-    if key.modifiers == []:
-        return key.key
-    else:
-        return "-".join(key.modifiers) + "-" + key.key
