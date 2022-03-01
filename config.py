42f9c2e13e1fdc2d405d013df1803b4fe4a21347
Hiroya-W <hyuyukun@gmail.com>
2022-03-01 11:31:37 +0900

    :+1: Keymap for inputting tilde

--- config.py
+++ config.py
@@ -38,6 +38,13 @@ key_map.extend(
     ]
 )
 
+# See Issue#1.
+# KeyMap for inputting ~(tilde) with Shift+`
+# Using xmodmap, assign KatakanaHiragana to ~(tilde).
+# In Xmodmap,
+# keycode 101 = asciitilde Romaji Hiragana_Katakana Romaji Hiragana_Katakana Romaji
+key_map[Key("Grave", Modifier.SHIFT)] = Key("KatakanaHiragana")
+
 key_map[Key("K", Modifier.ALT)] = Key("Up")
 key_map[Key("Comma", Modifier.ALT)] = Key("Down")
 key_map[Key("M", Modifier.ALT)] = Key("Left")
@@ -50,8 +57,6 @@ key_map[Key("G", Modifier.ALT)] = Key("KPAsterisk")
 key_map[Key("H", Modifier.ALT)] = Key("KPPlus")
 key_map[Key("B", Modifier.ALT)] = Key("KPSlash")
 key_map[Key("N", Modifier.ALT)] = Key("KPMinus")
-
-key_map[Key("Grave", Modifier.SHIFT)] = Key("Grave", Modifier.ALT)
 
 define_modmap({"CapsLock": "Control_L", "Alt_R": "Super_L"})
 define_keymap({}, key_map.dump(), "Global")
